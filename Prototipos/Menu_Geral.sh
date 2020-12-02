ee8746d5a6bf400caaa55af668d7fe4fc5a75d4f
samir <samir.souza@academico.ifpb.edu.br>
2020-12-02 20:13:08 -0300

    Ajustes nos Menus

--- Prototipos/Menu_Geral.sh
+++ Prototipos/Menu_Geral.sh
@@ -2,30 +2,21 @@
 
 Menu() {
 clear
-echo " Escolha uma maquina:"
-echo " 1.Maquina-1"
-echo " 2.Maquina-2"
-echo " 3.Maquina-3"
-echo
-echo -n "Esolha uma opção: "
-read OPTION
-echo
-case $OPTION in
-    1) echo "Você escolheu a opção 1" 
-	    ssh -t samir@192.168.1.7 -p 22 ./Teste_Menu_Parte-1.sh;;
-    2) echo "Você escolheu a opção 2" ;;
-    3) echo "Você escolheu a opção 3" ;;
-    *) echo "Opção inválida" ;;
-esac
 
-echo
-echo -n "Sair? [S/N] "
+echo "Bem Vindo!"
+read -p "Deseja fazer a conexao SSH? [S/N]" SSH
+case $SSH in
+	s|S"") read -p "Digite o nome da maquina quer deseja acessa: " name
+		read -p "Agora digite o endereço IP dela: " ip
+		ssh -t ${name}@${ip} -p 22 ./Menu_Secundario.sh ;;
+esac
+echo -n "Deseja sair? [S/N] "
 read SAIR
 echo
 case $SAIR in
-    s|S|"") exit ;;
-    n|N|"") Menu ;;
-    *) echo "Opção inválida" ;;
+	s|S"") exit ;;
+	n|N"") Menu ;;
+	*) echo "Opçao invalida" ;;
 esac
 }
 
